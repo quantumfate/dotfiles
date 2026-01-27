087ef89c880a35c7a6d6c49ac8791cf00ce65793
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-01-27 04:23:37 +0100

    Updated theme

---
+++ .chezmoiscripts/run_once_after_00-finalise-theme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Applying spotify theme."
+spicetify apply
+
+echo "Setting Catppuccin Papirus older theme."
+papirus-folders -C cat-macchiato-mauve --theme Papirus-Dark
--- .chezmoiscripts/run_once_before_05-install-yay.sh
+++ .chezmoiscripts/run_once_before_05-install-yay.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if command -v pacman >/dev/null 2>&1; then
+  if command -v yay >/dev/null 2>&1; then
+    exit 0
+  fi
   echo "Installing yay package manager."
   sudo pacman -S --noconfirm yay
 fi
