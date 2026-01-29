40f9954d845226786a758c241e85606cb573be53
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-01-29 18:30:16 +0100

    User.js

--- user.js
+++ user.js
@@ -1,3 +1,4 @@
+
 //
 /* You may copy+paste this file and use it as it is.
  *
@@ -62,8 +63,6 @@ user_pref("network.dns.disablePrefetchFromHTTPS", true);
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("browser.places.speculativeConnect.enabled", false);
 user_pref("network.prefetch-next", false);
-user_pref("network.predictor.enabled", false);
-user_pref("network.predictor.enable-prefetch", false);
 
 /****************************************************************************
  * SECTION: SECUREFOX                                                       *
@@ -73,14 +72,11 @@ user_pref("browser.contentblocking.category", "strict");
 user_pref("browser.download.start_downloads_in_tmp_dir", true);
 user_pref("browser.uitour.enabled", false);
 user_pref("privacy.globalprivacycontrol.enabled", true);
-user_pref("privacy.trackingprotection.allow_list.baseline.enabled", true);
-user_pref("privacy.trackingprotection.allow_list.convenience.enabled", true);
 
 /** OCSP & CERTS / HPKP ***/
 user_pref("security.OCSP.enabled", 0);
 user_pref("privacy.antitracking.isolateContentScriptResources", true);
 user_pref("security.csp.reporting.enabled", false);
-user_pref("security.pki.crlite_mode", 2);
 
 /** SSL / TLS ***/
 user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
@@ -170,11 +166,10 @@ user_pref("browser.tabs.crashReporting.sendReport", false);
  * SECTION: PESKYFOX                                                        *
 ****************************************************************************/
 /** MOZILLA UI ***/
-user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.getAddons.showPane", true);
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("browser.discovery.enabled", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
-user_pref("browser.newtabpage.activity-stream.default.sites", "");
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 user_pref("browser.preferences.moreFromMozilla", false);
@@ -202,7 +197,6 @@ user_pref("full-screen-api.warning.timeout", 0);
 
 /** URL BAR ***/
 user_pref("browser.urlbar.trending.featureGate", false);
-user_pref("dom.text_fragments.create_text_fragment.enabled", true);
 
 /** NEW TAB PAGE ***/
 user_pref("browser.newtabpage.activity-stream.default.sites", "");
@@ -223,120 +217,12 @@ user_pref("browser.menu.showViewImageInfo", true);
 user_pref("findbar.highlightAll", true);
 user_pref("layout.word_select.eat_space_to_next_word", false);
 
-
-/****************************************************************************
- * START: ZEN-SPECIFIC OVERRIDES                                            *
-****************************************************************************/
-// Remove the slashes to enable the prefs
-
-// PREF: re-enable Windows efficiency mode
-//user_pref("dom.ipc.processPriorityManager.backgroundUsesEcoQoS", true);
-
-// PREF: disable new tab preload since they are off by default
-user_pref("browser.newtab.preload", false);
-
-// PREF: show Enhance Tracking Protection shield in URL bar
-// Currently bugged if you click to view what's blocked
-user_pref("zen.urlbar.show-protections-icon", true);
-
-// PREF: Disable the Picture in picture pop-out when changing tabs
-user_pref("media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled", false);
-
 /****************************************************************************
  * START: MY OVERRIDES                                                      *
 ****************************************************************************/
 // visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
 // visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
 // Enter your personal overrides below this line:
-
-/** SETUP ON FIRST INSTALLATION ***/
-//user_pref("network.trr.uri", "https://dns.nextdns.io/******/Firefox"); // TRR/DoH
-
-/** FASTFOX ***/
-user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
-
-// SPECULATIVE LOADING WITHOUT PREDICTOR
-user_pref("network.http.speculative-parallel-limit", 20);
-//user_pref("network.dns.disablePrefetch", false);
-//user_pref("network.dns.disablePrefetchFromHTTPS", false);
-//user_pref("dom.prefetch_dns_for_anchor_https_document", true); 
-user_pref("browser.urlbar.speculativeConnect.enabled", true);
-user_pref("browser.places.speculativeConnect.enabled", true);
-user_pref("network.prefetch-next", true);
-user_pref("network.http.max-persistent-connections-per-server", 20); // increase download connections
-
-/** SECUREFOX ***/
-user_pref("privacy.trackingprotection.allow_list.convenience.enabled", false); // disable Strict allowlist of convenience features
-user_pref("signon.rememberSignons", false); // disable password manager
-user_pref("extensions.formautofill.addresses.enabled", false); // disable address manager
-user_pref("extensions.formautofill.creditCards.enabled", false); // disable credit card manager
-user_pref("browser.urlbar.suggest.recentsearches", false);  // unselect "Show recent searches" for clean UI
-user_pref("browser.urlbar.showSearchSuggestionsFirst", false); // unselect "Show search suggestions ahead of browsing history in address bar results" for clean UI
-//user_pref("browser.urlbar.groupLabels.enabled", false); // hide Firefox Suggest label in URL dropdown box
-user_pref("signon.management.page.breach-alerts.enabled", false); // extra hardening
-user_pref("signon.autofillForms", false); // unselect "Autofill logins and passwords" for clean UI
-user_pref("signon.generation.enabled", false); // unselect "Suggest and generate strong passwords" for clean UI
-user_pref("signon.firefoxRelay.feature", ""); // unselect suggestions from Firefox Relay for clean UI
-user_pref("browser.safebrowsing.downloads.enabled", false); // deny SB to scan downloads to identify suspicious files; local checks only
-user_pref("browser.safebrowsing.downloads.remote.url", ""); // enforce no remote checks for downloads by SB
-user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false); // clean up UI; not needed in user.js if remote downloads are disabled
-user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false); // clean up UI; not needed in user.js if remote downloads are disabled
-user_pref("browser.safebrowsing.allowOverride", false); // do not allow user to override SB
-user_pref("browser.search.update", false); // do not update opensearch engines
-user_pref("network.trr.confirmationNS", "skip"); // skip TRR confirmation request
-user_pref("extensions.webextensions.restrictedDomains", ""); // remove Mozilla domains so adblocker works on pages
-user_pref("identity.fxaccounts.enabled", true); // enable Firefox Sync
-user_pref("browser.firefox-view.feature-tour", "{\"screen\":\"\",\"complete\":true}"); // disable the Firefox View tour from popping up for new profiles
-user_pref("accessibility.force_disabled", 1); // disable Accessibility features
-user_pref("security.cert_pinning.enforcement_level", 2); // strict public key pinning
-user_pref("captivedetect.canonicalURL", ""); // disable captive portal detection
-user_pref("network.captive-portal-service.enabled", false); // disable captive portal detection
-user_pref("network.connectivity-service.enabled", false); // disable captive portal detection
-user_pref("browser.download.enableDeletePrivate", true); // Delete files downloaded in private browsing when all private windows are closed
-user_pref("browser.download.deletePrivateChosen", true); // Delete files downloaded in private browsing when all private windows are closed
-user_pref("browser.download.deletePrivate", true); // Delete files downloaded in private browsing when all private windows are closed
-
-/** PESKYFOX ***/
-user_pref("devtools.accessibility.enabled", false); // removes un-needed "Inspect Accessibility Properties" on right-click
-user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false); // Settings>Home>Firefox Home Content>Recent Activity>Shortcuts>Sponsored shortcuts
-user_pref("browser.newtabpage.activity-stream.showSponsored", false); // Settings>Home>Firefox Home Content>Recent Activity>Recommended by Pocket>Sponsored Stories  
-user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false); // Settings>Home>Firefox Home Content>Recent Activity>Bookmarks
-user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false); // Settings>Home>Firefox Home Content>Recent Activity>Most Recent Download
-user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false); // Settings>Home>Firefox Home Content>Recent Activity>Visited Pages
-user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false); // Settings>Home>Firefox Home Content>Recent Activity>Pages Saved to Pocket
-//user_pref("browser.download.useDownloadDir", true); // use direct downloads
-//user_pref("browser.download.folderList", 0); // 0=desktop, 1=downloads, 2=last used
-user_pref("browser.toolbars.bookmarks.visibility", "never"); // always hide bookmark bar
-user_pref("browser.startup.homepage_override.mstone", "ignore"); // What's New page after updates; master switch
-user_pref("browser.urlbar.suggest.history", false); // Browsing history; hide URL bar dropdown suggestions
-user_pref("browser.urlbar.suggest.bookmark", false); // Bookmarks; hide URL bar dropdown suggestions
-user_pref("browser.urlbar.suggest.openpage", false); // Open tabs; hide URL bar dropdown suggestions
-user_pref("browser.urlbar.suggest.topsites", false); // Shortcuts; disable dropdown suggestions with empty query
-user_pref("browser.urlbar.suggest.engines", false); // Search engines; tab-to-search
-user_pref("browser.urlbar.quicksuggest.enabled", false); // hide Firefox Suggest UI in the settings
-//user_pref("browser.urlbar.maxRichResults", 1); // minimum suggestion needed for URL bar autofill
-user_pref("browser.bookmarks.max_backups", 0); // minimize disk use; manually back-up
-user_pref("view_source.wrap_long_lines", true);  // wrap source lines
-user_pref("devtools.debugger.ui.editor-wrapping", true);  // wrap lines in devtools
-user_pref("browser.zoom.full", false); // text-only zoom, not all elements on page
-//user_pref("pdfjs.sidebarViewOnLoad", 2); // force showing of Table of Contents in sidebar for PDFs (if available)
-user_pref("layout.word_select.eat_space_to_next_word", false); // do not select the space next to a word when selecting a word
-//user_pref("browser.tabs.loadInBackground", false); // CTRL+SHIFT+CLICK for background tabs; Settings>General>Tabs>When you open a link, image or media in a new tab, switch to it immediately
-user_pref("browser.tabs.loadBookmarksInTabs", true); // force bookmarks to open in a new tab, not the current tab
-user_pref("ui.key.menuAccessKey", 0); // remove underlined characters from various settings
-user_pref("general.autoScroll", false); // disable unintentional behavior for middle click
-user_pref("ui.SpellCheckerUnderlineStyle", 1); // [HIDDEN] dots for spell check errors
-user_pref("media.videocontrols.picture-in-picture.display-text-tracks.size", "small"); // PiP
-user_pref("media.videocontrols.picture-in-picture.urlbar-button.enabled", false); // PiP in address bar
-user_pref("reader.parse-on-load.enabled", false); // disable reader mode
-//user_pref("reader.color_scheme", "auto"); // match system theme for when reader is enabled
-//user_pref("browser.urlbar.openintab", true); // stay on current site and open new tab when typing in URL bar
-
-/** DELETE IF NOT LINUX LAPTOP ***/
-//user_pref("network.trr.mode", 2); // enable TRR (with System fallback)
-//user_pref("network.trr.max-fails", 5); // lower max attempts to use DoH
-//user_pref("geo.provider.use_geoclue", false); // [LINUX]
-//user_pref("pdfjs.defaultZoomValue", "page-width"); // PDF zoom level
 
 
 
@@ -346,7 +232,13 @@ user_pref("reader.parse-on-load.enabled", false); // disable reader mode
 // visit https://github.com/yokoffing/Betterfox/blob/main/Smoothfox.js
 // Enter your scrolling overrides below this line:
 
-
+// only sharpen scrolling
+user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("mousewheel.min_line_scroll_amount", 10); // adjust this number to your liking; default=5
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 80); // default=50
+user_pref("general.smoothScroll.currentVelocityWeighting", "0.15"); // default=.25
+user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6"); // default=.4
 
 /****************************************************************************
  * END: BETTERFOX                                                           *
